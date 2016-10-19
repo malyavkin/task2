329641beb55575b131bfbf0c24d1bcfa4e658817
Alexey Malyavkin <a@malyavk.in>
2016-10-19 16:49:49 +0300

    add task execution

--- task2/rest/views.py
+++ task2/rest/views.py
@@ -1,10 +1,10 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from task2.rest.models import TaskResult
-
 from rest_framework import viewsets
-from task2.rest.serializers import TaskSerializer
+from task2.rest.serializers import *
+from task2.rest.utils.math_utils import *
+from task2.rest.utils.ping_utils import *
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -24,15 +24,24 @@ class TaskViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         """
         initialize task
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
         """
         data = request.data
-        data.user_id = request.user
+        data["user_id"] = request.user
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user, status="pending")
+        # task execution
+        task_result = 0
+        post_args = request.data["args"]
+        if request.data["task_name"] == "nth_prime":
+            task_result = find_nth_prime(int(post_args))
+        elif request.data["task_name"] == "factorize":
+            task_result = factorize(int(post_args))
+        elif request.data["task_name"] == "ping":
+            argv = post_args.split(" ")
+            hostname = argv[0]
+            times = int(argv[1])
+            task_result = ping(hostname, times)
+
+        serializer.save(user=request.user, task_result=task_result)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
