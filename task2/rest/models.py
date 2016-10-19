9aaedabe1e80aba339126bcdd029d540c37d6cc2
Alexey Malyavkin <a@malyavk.in>
2016-10-19 14:48:42 +0300

    fix model

--- task2/rest/models.py
+++ task2/rest/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 
 class TaskResult(models.Model):
     task_name = models.CharField(max_length=20)
     task_result = models.TextField(max_length=20)
-    user = User()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
