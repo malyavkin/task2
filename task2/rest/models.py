cad6d2c7649e1de6865f0aea9aac151e9b00d71d
Alexey Malyavkin <a@malyavk.in>
2016-10-19 16:49:10 +0300

    make task name enum

--- task2/rest/models.py
+++ task2/rest/models.py
@@ -3,8 +3,11 @@ from django.conf import settings
 
 
 class TaskResult(models.Model):
-    task_name = models.CharField(max_length=20)
-    args = models.TextField(max_length=4096)
-    task_result = models.TextField(max_length=4096)
+    task_name = models.CharField(max_length=20, choices=(
+        ('nth_prime', 'Nth prime'),
+        ('factorize', 'Factorize'),
+        ('ping', 'Ping'),
+    ))
+    args = models.TextField(max_length=4096, default="")
+    task_result = models.TextField(max_length=4096, default="")
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    status = models.CharField(max_length=10)
