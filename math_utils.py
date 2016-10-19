b605284000aa48cf3d8114e5b57f2fc79052b835
Alexey Malyavkin <a@malyavk.in>
2016-10-19 14:11:00 +0300

    docstrings

--- math_utils.py
+++ math_utils.py
@@ -2,6 +2,11 @@ from math import sqrt
 
 
 def is_prime(n):
+    """
+    Test if number is prime
+    :param n: number in question
+    :return: if number is prime
+    """
     j = 2
     while j <= int(sqrt(n)):
         if n % j == 0:
@@ -11,6 +16,11 @@ def is_prime(n):
 
 
 def find_nth_prime(n):
+    """
+    Find Nth prime number
+    :param n: number in question
+    :return: nth prime number
+    """
     prime_count = 0
     num = 1
     while prime_count < n:
@@ -21,6 +31,11 @@ def find_nth_prime(n):
 
 
 def factorize(n):
+    """
+    returns list of prime factors of N
+    :param n:
+    :return: returns list of prime factors of N
+    """
     factors = []
     while n > 1:
         for i in range(2, n + 1):
