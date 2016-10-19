beeeeebbe2ba9900a268364d199e58573eb35ac6
Alexey Malyavkin <a@malyavk.in>
2016-10-19 16:45:23 +0300

    make ping return command output

--- task2/rest/utils/ping_utils.py
+++ task2/rest/utils/ping_utils.py
@@ -1,11 +1,17 @@
 import platform
 import os
-
+import subprocess
 
 def ping(hostname, times):
+    """
+    pings given hostname given # of times and returns cmd output
+    :param hostname: hostname to ping
+    :param times: #of times to ping
+    :return:
+    """
     pl = platform.system()
     # assume every platform other than Windows uses -c for ping count
     count_arg = '-n' if pl == 'Windows' else '-c'
-    cmd = " ".join(['ping', count_arg, str(times), hostname])
-    result = os.system(cmd)
-    return result
+    proc = subprocess.Popen(["ping", count_arg, str(times), hostname], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    return out
